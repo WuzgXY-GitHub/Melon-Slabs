216a59f95a9ce95a1ae7bec5f43036a442582523
unknown <gregoirebrougher@gmail.com>
2020-08-05 12:28:27 -0700

    Added creative tab, made melon slabs compostable.

--- src/main/java/net/melon/slabs/MelonSlabs.java
+++ src/main/java/net/melon/slabs/MelonSlabs.java
@@ -1,20 +1,29 @@
 package net.melon.slabs;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class MelonSlabs implements ModInitializer {
+    //Blocks
+    private static final Block MELON_SLAB_BLOCK = new MelonSlab();
 
-    public static final Block MELON_SLAB = new MelonSlab();
+    //Item group
+    private static final ItemGroup GROUP = FabricItemGroupBuilder.build(new Identifier("melonslabs", "group"), () -> new ItemStack(MelonSlabs.MELON_SLAB));
+
+    //Items
+    public static final Item MELON_SLAB = new BlockItem(MELON_SLAB_BLOCK, new Item.Settings().group(GROUP));
 
     @Override
     public void onInitialize() {
-        Registry.register(Registry.BLOCK, "melonslabs:melon_slab", MELON_SLAB);
-        Registry.register(Registry.ITEM, "melonslabs:melon_slab", new BlockItem(MELON_SLAB, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+        Registry.register(Registry.BLOCK, "melonslabs:melon_slab", MELON_SLAB_BLOCK);
+        Registry.register(Registry.ITEM, "melonslabs:melon_slab", MELON_SLAB);
     }
     
 }
